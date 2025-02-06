c8859721974508e46a695e6efdedace2a2eb45b1
Trimboli <e2258971@cmaisonneuve.qc.ca>
2025-02-06 11:16:24 -0500

    s3c2 ajout section populaire et quelques autres changements de style

--- index.php
+++ index.php
@@ -36,5 +36,16 @@
       </div>
     </div>
   </section>
+
+  <section class="populaire">
+    <div class="global">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+          <article class="populaire__article">
+            <h2 class="populaire__titre"><?php the_title(); ?></h2>
+            <div class="populaire__contenu"><?php echo wp_trim_words(get_the_content(), 10, "..."); ?></div>
+          </article>
+        <?php endwhile; endif; ?>
+    </div>
+  </section>
   
   <?php get_footer(); ?>
