f1de24ccae2b47b9504050f9deb7c66a51010d5b
Trimboli <e2258971@cmaisonneuve.qc.ca>
2025-03-25 09:34:45 -0400

    s8c1 ajout 404.php

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php
+/** 
+ * modèle front-page.php permet d'afficher la page d'accueil
+ * 
+*/
+?>
+
+<?php get_header() ?>
+<h1 class="hidde">404.php</h1>
+    <?php get_template_part("gabarit/hero") ?>
+
+    <section class="populaire">
+        <div class="boiteflex global">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php if (in_category('galerie')){
+                the_content();
+            } else { ?>         
+                <?php get_template_part("gabarit/carte"); ?>
+            <?php } ?>
+            <?php endwhile; endif; ?>
+        </div>
+    </section>
+
+<?php get_footer(); ?>
+</body>
+</html>
