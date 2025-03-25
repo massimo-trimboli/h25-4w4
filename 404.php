9e1194c7aaa948d68d70e5f5ee1e024775d57302
Trimboli <e2258971@cmaisonneuve.qc.ca>
2025-03-25 10:31:05 -0400

    s8c1 debut branche tp2 et modification 404.php

--- 404.php
+++ 404.php
@@ -7,17 +7,49 @@
 
 <?php get_header() ?>
 <h1 class="hidde">404.php</h1>
-    <?php get_template_part("gabarit/hero") ?>
+    
+<?php $hero_author = get_theme_mod('hero_author', 'Default Title'); 
+      $hero_background = get_theme_mod('hero_background', 'Default Title');
+      $hero_email = get_theme_mod('hero_email', 'email@email.com'); 
+      $hero_phone = get_theme_mod('hero_phone', '123-456-7890'); 
+      $hero_adress = get_theme_mod('hero_adress', '123 rue adresse ville'); 
+      
+      $hero_text_color = get_theme_mod('hero_text_color', 'black');
+      $hero_svg_color = get_theme_mod('hero_svg_color', '000000');
+      $svg_color_poundless = substr($hero_svg_color, 1);
+?>
+<section class="hero" style="background-image: url(<?= $hero_background ?>); background-repeat: no-repeat; color: <?= $hero_text_color ?>;">
+        <div class="hero__contenu global">
+            <h1 class="hero__titre">
+                404
+            </h1>
+            <p class="hero__description">
+            la page n'a pas pu être trouvée
+            </p>
 
-    <section class="populaire">
-        <div class="boiteflex global">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php if (in_category('galerie')){
-                the_content();
-            } else { ?>         
-                <?php get_template_part("gabarit/carte"); ?>
-            <?php } ?>
-            <?php endwhile; endif; ?>
+            <a href="">
+                <button class="hero__bouton .retour">retour page principale</button>
+            </a>
+
+            <a href="" class="hero__courriel">
+                <?= $hero_email ?>
+            </a>
+            <a href="" class="hero__phone">
+                <?= $hero_phone ?>
+            </a>
+            <a href="https://www.google.com/maps/place/<?= $hero_adress ?>" target="_blank" rel="noreferrer noopener" class="hero__adress">
+                <?= $hero_adress ?>
+            </a>
+            <button class="hero__bouton">
+                Inscription
+            </button>
+            <div class="hero__icone-app">
+                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=<?= $svg_color_poundless ?>" width="20" height="20" style="color: red;">
+                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=<?= $svg_color_poundless ?>" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=<?= $svg_color_poundless ?>" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=<?= $svg_color_poundless ?>" width="20" height="20">
+            </div>
+            <p class="author">Autheur: <?= $hero_author ?></p>
         </div>
     </section>
 
