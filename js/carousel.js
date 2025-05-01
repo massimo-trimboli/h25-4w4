c38187a1a34bc368623b193f0d4662592ff8a9d1
Trimboli <e2258971@cmaisonneuve.qc.ca>
2025-05-01 10:23:32 -0400

    s13c2 la gestion d'animations dans carousel.js plus modulaire. on n'a qua rajouter un nom de classe dans le array pour rajouter une nouvelle animation

--- js/carousel.js
+++ js/carousel.js
@@ -1,28 +1,36 @@
 (function(){
     let lesBoutons = document.querySelectorAll(".radio-carousel");
     let lesSlides = document.querySelectorAll(".hero__carousel");
+
+    //définir les animations ici
+    let lesAnimations = [
+        "spin-anim",
+        "slot-machine-anim-y",
+        "slot-machine-anim-x"
+    ]
+
     //en allumer un au debut
     for(let uneSlide of lesSlides){
         uneSlide.classList.remove("carouselActif");
     }
     lesBoutons[0].checked = true;
-    switchSlide();
+    switchSlide(lesAnimations);
 
     //automatisation
-    let interval = setInterval(autoSwitch, 30000);
+    let interval = setInterval(function(){autoSwitch(lesAnimations);}, 30000);
 
     //rajouter le event listner
     for(let bouton of lesBoutons){
-        bouton.addEventListener("change", switchSlide);
+        bouton.addEventListener("change", function(){switchSlide(lesAnimations)});
     }
 
 
     //pour lanimation du titre lorse quon change de slide
-    animationListner();
+    animationListner(lesAnimations);
 })()
 
 //fonction pour switcher automatiquement
-function autoSwitch(){
+function autoSwitch(lesAnimations){
     let lesBoutons = document.querySelectorAll(".radio-carousel");
 
     //trouver bouton checked
@@ -47,11 +55,11 @@ function autoSwitch(){
         }
     }
 
-    switchSlide();
+    switchSlide(lesAnimations);
 }
 
 //fonction qui switch a la slide correspondant au bouton checked
-function switchSlide(){
+function switchSlide(lesAnimations){
     let lesBoutons = document.querySelectorAll(".radio-carousel");
     let lesSlides = document.querySelectorAll(".hero__carousel");
 
@@ -76,62 +84,44 @@ function switchSlide(){
     }
 
     //faire animer le titre
-    animTitre();
+    animTitre(lesAnimations);
 }
 
 
 //animer le titre
 //ajout listner pour enlever la classe animation
-function animationListner(){
-    
-    // let titre = document.querySelector(".hero__titre");
-
-    // titre.addEventListener("animationend", function(){
-    //     titre.classList.remove("spin-anim");
-    // })
-    
-
+function animationListner(lesAnimations){
    let contenuHero = document.querySelector(".hero__contenu");
    let enfants = contenuHero.children;
 
    for(let child of enfants){
     child.addEventListener("animationend", function(){
-        child.classList.remove("spin-anim");
-        child.classList.remove("slot-machine-anim-y");
-        child.classList.remove("slot-machine-anim-x");
+        for(uneAnimation of lesAnimations){
+            child.classList.remove(uneAnimation);
+        }
     })
    }
 }
 //doner la classe animation
-function animTitre(){
-    
-    // let titre = document.querySelector(".hero__titre");
-    // if(titre!=null){
-    //     titre.classList.add("spin-anim");
-    // }
-    
-
-
+function animTitre(lesAnimations){
     let contenuHero = document.querySelector(".hero__contenu");
     let enfants = contenuHero.children;
 
-    if(contenuHero.dataset.anim == 0){
-        for(let child of enfants){
-            child.classList.add("spin-anim");
+    // let lesAnimations = [
+    //     "spin-anim",
+    //     "slot-machine-anim-y",
+    //     "slot-machine-anim-x"
+    // ]
 
-            contenuHero.dataset.anim = 1;
-        }
-    } else if(contenuHero.dataset.anim == 1){
-        for(let child of enfants){
-            child.classList.add("slot-machine-anim-y");
+    for(let child of enfants){
+        child.classList.add(
+            lesAnimations[contenuHero.dataset.anim]
+        );
+    }
 
-            contenuHero.dataset.anim = 2;
-        }
-    } else if(contenuHero.dataset.anim == 2){
-        for(let child of enfants){
-            child.classList.add("slot-machine-anim-x");
-
-            contenuHero.dataset.anim = 0;
-        }
+    //incrementer animation
+    contenuHero.dataset.anim++;
+    if(contenuHero.dataset.anim >= lesAnimations.length){
+        contenuHero.dataset.anim = 0
     }
 }
