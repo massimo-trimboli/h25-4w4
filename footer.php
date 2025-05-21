4a19cd33047ec424e7a9e36bd96aa7ed2e86813f
massimo <masst123543@gmail.com>
2025-05-20 20:48:54 -0400

    ajout fonction icone dans footer

--- footer.php
+++ footer.php
@@ -66,11 +66,8 @@
              </style>
         </section>
         <section class="piedpage__s3">
-        <div class="piedpage__s3__icone-app">
-                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=<?= $svg_color_poundless ?>" width="20" height="20" style="color: red;">
-                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=<?= $svg_color_poundless ?>" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=<?= $svg_color_poundless ?>" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=<?= $svg_color_poundless ?>" width="20" height="20">
+            <div class="piedpage__s3__icone-app">
+                <?php iconesSociaux($svg_color_poundless) ?>
             </div>
         </section>
 
