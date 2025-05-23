60ffd78a5a68bda0bdd3fd3f88457c3fa6d5a903
Trimboli <e2258971@cmaisonneuve.qc.ca>
2025-05-23 10:51:49 -0400

    exam final rajout champs ACF

--- template-pays.php
+++ template-pays.php
@@ -10,6 +10,14 @@ Template Name: Template pays
         <?php if(have_posts()): ?>
        <h1><?php the_title(); ?></h1>
        <h3>Plongez au cœur de l’aventure et laissez-vous emporter par l’appel du large ! Notre planète regorge de <span class="gras">destinations incroyables</span>, chacune promettant une expérience unique et mémorable. Que vous rêviez de plages idylliques baignées de soleil, de sommets majestueux invitant à la randonnée, de villes vibrantes d’histoire et de modernité, ou de rencontres culturelles authentiques, il y a un pays fait pour vous.</h3>
+       <!-- champs ACF -->
+       <p>
+            Conférencier: <?= the_field("conferencier"); ?>
+            <br>
+            lieu de l'évenemetn:  <?= the_field("lieu"); ?>
+            <br>
+            date de l'évenemetn: <?= the_field("date"); ?>
+        </p>
         <?php the_content(); ?>
         <?php endif; ?>
     </div>
